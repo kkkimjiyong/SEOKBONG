f1f14bb449eb2d782112688fd57d954e51efabf3
김지용 <wldyddkssud@naver.com>
2023-03-17 17:06:20 +0900

    feat: prop-types 패키지 설치로 오류해결

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Router from "./shared/Router";
 import ReactGA from "react-ga";
 import { createBrowserHistory } from "@remix-run/router";
 function App() {
-  const gaTrackingId = import.meta.env.REACT_APP_GA_TRACKING_ID; // 환경 변수에 저장된 추적ID 가져오기
+  const gaTrackingId = import.meta.env.VITE_GA_TRACKING_ID; // 환경 변수에 저장된 추적ID 가져오기
   ReactGA.initialize(gaTrackingId, { debug: true }); // react-ga 초기화 및 debug 사용
 
   const history = createBrowserHistory();
